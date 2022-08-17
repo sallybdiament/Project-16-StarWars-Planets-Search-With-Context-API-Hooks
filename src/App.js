95893e15c124a7306fb904b1bd08807fe286884c
Sally Diament <sally.barzilai@gmail.com>
2022-08-17 14:37:46 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import TableContext from './context/TableContext';
+import Starwars from './Starwars';
 
 function App() {
+  const contextValue = 'Hello';
   return (
-    <span>Hello, Sally!</span>
+    <TableContext.Provider value={ contextValue }>
+      <Starwars />
+    </TableContext.Provider>
   );
 }
 
---
+++ src/Starwars.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TableContext from './context/TableContext';
+
+function Starwars() {
+  return (
+    <p>Hello</p>
+  );
+}
+
+export default Starwars;
